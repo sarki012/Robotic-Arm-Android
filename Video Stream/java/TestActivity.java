6eb797785d3387dc335c5e5ce2567d5ecc3b449f
sarki012 <60797267+sarki012@users.noreply.github.com>
2022-11-01 07:45:43 -0500

    Floating Window Over xml Layout
    Dual Module Program.

---
+++ Video Stream/java/TestActivity.java
@@ -0,0 +1,18 @@
+package com.esark.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esark.test_shadow.R;
+
+//import com.esark.test_shadow.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
